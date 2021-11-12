ebda3169ace779eb03d58ba2c1d68ea83a7e2adb
raiguard <caleb.heuer@gmail.com>
2021-11-12 14:14:13 -0700

    Rearrange colors and add critical priority after feedback

--- src/constants.lua
+++ src/constants.lua
@@ -2,9 +2,10 @@ local constants = {}
 
 constants.task_status = {
   not_started = { color = "black", label = { "gui.tlst-status-not-started" } },
-  in_progress_high = { color = "green", label = { "gui.tlst-status-in-progress-high" } },
-  in_progress_low = { color = "yellow", label = { "gui.tlst-status-in-progress-low" } },
-  paused = { color = "red", label = { "gui.tlst-status-paused" } },
+  in_progress_critical = { color = "red", label = { "gui.tlst-status-in-progress-critical" } },
+  in_progress_high = { color = "yellow", label = { "gui.tlst-status-in-progress-high" } },
+  in_progress_low = { color = "green", label = { "gui.tlst-status-in-progress-low" } },
+  paused = { color = "white", label = { "gui.tlst-status-paused" } },
 }
 local i = 0
 for _, status_info in pairs(constants.task_status) do
