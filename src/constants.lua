5bf30423edc11fe2a6449733b01195dd90e99a4e
raiguard <caleb.heuer@gmail.com>
2021-11-12 13:54:46 -0700

    Add paused status, rearrange statuses

--- src/constants.lua
+++ src/constants.lua
@@ -1,9 +1,15 @@
 local constants = {}
 
 constants.task_status = {
-  not_started = { color = "red", label = { "gui.tlst-status-not-started" }, index = 1 },
-  in_progress_low = { color = "yellow", label = { "gui.tlst-status-in-progress-low" }, index = 2 },
-  in_progress_high = { color = "green", label = { "gui.tlst-status-in-progress-high" }, index = 3 },
+  not_started = { color = "black", label = { "gui.tlst-status-not-started" } },
+  in_progress_high = { color = "green", label = { "gui.tlst-status-in-progress-high" } },
+  in_progress_low = { color = "yellow", label = { "gui.tlst-status-in-progress-low" } },
+  paused = { color = "red", label = { "gui.tlst-status-paused" } },
 }
+local i = 0
+for _, status_info in pairs(constants.task_status) do
+  i = i + 1
+  status_info.index = i
+end
 
 return constants
